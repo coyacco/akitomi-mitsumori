b77266084b4d5779b6a5060dd2a1112c1461abe7
Soji Seta <coyacco@gmail.com>
2026-04-29 16:38:47 +0900

    fix: vite用実装をマスク

--- client/src/loadServerAddress.ts
+++ client/src/loadServerAddress.ts
@@ -2,7 +2,8 @@ import { readTextFile } from "@tauri-apps/plugin-fs";
 import { appConfigDir, join } from "@tauri-apps/api/path";
 
 export async function loadServerAddress() {
-  return "http://localhost:3001";
+  // vite ではローカルファイルが読めないので、開発中は固定値を返す
+  // return "http://localhost:3001";
   const dir = await appConfigDir();
   const path = await join(dir, "server-config.json");
 
